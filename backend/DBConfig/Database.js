90a3397f0a64ceeabc2c3e018e3b8f94bd1db47a
Mathankumar R <rmathankumarcpm@gmail.com>
2022-10-20 12:21:23 +0530

    First commit

--- backend/DBConfig/Database.js
+++ backend/DBConfig/Database.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: "Your host name",
-    USER: "User name",
-    PASSWORD: "password",
-    DB: "Ddatabase name",
+    HOST: "sql6.freemysqlhosting.net",
+    USER: "sql6527893",
+    PASSWORD: "ynkHmjM3xp",
+    DB: "sql6527893",
     dialect: "mysql"
   };
